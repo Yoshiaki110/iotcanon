e766f9df7af1b31152c2b5c8a085dbb64d56acfc
yoshiaki110 <yoshiaki110@gmail.com>
2018-04-15 16:30:56 +0900

    twilio call

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+var setting = require('./setting.js');
+var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
+var TWILIO_CALL_URL = setting.TWILIO_CALL_URL;
+var TWILIO_CALL_BODY = setting.TWILIO_CALL_BODY;
+
 var app = express();
 var values = [];
 
@@ -15,6 +20,22 @@ app.post('/test', function(req, res) {
   console.log('<>POST /test');
   console.log(req.body);
   res.sendStatus(200);
+});
+app.get('/api/setting', function(req, res) {
+  console.log('<>GET /api/setting');
+//  console.log(req);
+  var id = req.url.split('?')[1];
+  var json = JSON.parse(fs.readFileSync(id + '.json', 'utf8'));
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  //res.sendStatus(200);
+  res.send(json);
+});
+app.post('/api/setting', function(req, res) {
+  console.log('<>POST /api/setting');
+//  console.log(req.body);
+  res.sendStatus(200);
+  fs.writeFile(req.body.id + '.json', JSON.stringify(req.body, null, '  '));
+  console.log(req.body.id + '.json');
 });
 app.post('/api/webiot', function(req, res) {
   console.log('<>POST /api/webiot');
@@ -38,6 +59,44 @@ app.post('/api/webiot', function(req, res) {
   console.log(req.body.id + ' = ' + req.body.value);
   res.sendStatus(200);
 });
+app.post('/api/move', function(req, res) {
+  console.log('<>POST /api/move');
+  var id = req.body.id;
+  var json = JSON.parse(fs.readFileSync(id + '.json', 'utf8'));
+  res.sendStatus(200);
+  if (json.enable && json.phone != '') {
+    var date = new Date();
+    var hour = date.getHours();
+    var dayOfWeek = date.getDay();
+    var call = false;
+    switch (dayOfWeek) {
+    case 0:
+      call = json.sun[hour];
+      break;
+    case 1:
+      call = json.mon[hour];
+      break;
+    case 2:
+      call = json.tue[hour];
+      break;
+    case 3:
+      call = json.wed[hour];
+      break;
+    case 4:
+      call = json.thu[hour];
+      break;
+    case 5:
+      call = json.fri[hour];
+      break;
+    case 6:
+      call = json.sat[hour];
+      break;
+    }
+    if (call) {
+      twilio(json.phone);
+    }
+  }
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
@@ -57,9 +116,26 @@ function postGDoc() {
     };
     request.post(options, function(error, response, body){});
   }
-  setTimeout(postGDoc, 60000);
+  setTimeout(postGDoc, 60*60*1000);
 }
-setTimeout(postGDoc, 1000);
+
+function twilio(phone) {
+  var headers = {
+    'Accept': '*/*',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+  var body = 'NumberToCall=%2B81' + phone.substr( 1);
+  console.log(body);
+  var url = TWILIO_CALL_URL;
+  request({
+    url: url,
+    method: 'POST',
+    headers: headers,
+    body: body,
+    json: false
+  });
+}
+setTimeout(postGDoc, 2*60*1000);
 
 /*
 {
---
+++ setting.js
@@ -0,0 +1,1 @@
+exports.TWILIO_CALL_URL = 'https://pistachio-dingo-9404.twil.io/makecall'
